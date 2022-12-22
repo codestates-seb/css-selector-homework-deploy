70379d3a1d12023c0ed16c2a3742228311a3c739
Hong Shik Branden Kim <kimploo@gmail.com>
2022-12-22 09:38:40 +0900

    initial commit

---
+++ precache-manifest.32c744a3cb0b0fbc94d950fd5e71da92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "251be0d0af3a3122830ac9c0b1a03e3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca501ea207e2f850a240",
+    "url": "/static/css/main.ae83b28d.chunk.css"
+  },
+  {
+    "revision": "ebcb144a43baccd8a9f1",
+    "url": "/static/js/2.cfffea70.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cfffea70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca501ea207e2f850a240",
+    "url": "/static/js/main.a0cfdc87.chunk.js"
+  },
+  {
+    "revision": "9dcc8fe9a088ffdbd6f6",
+    "url": "/static/js/runtime-main.fd36d1f4.js"
+  }
+]);
